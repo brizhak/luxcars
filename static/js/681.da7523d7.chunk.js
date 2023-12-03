022e00154a0c0daff805c57a54285f39c9dab876
brizhak <122231868+brizhak@users.noreply.github.com>
2023-12-03 20:16:07 +0000

    Deploying to gh-pages from @ brizhak/luxcars@29804b121d5e7bbd31ff0966a0abd999006e472f 🚀

---
+++ static/js/681.da7523d7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkluxcars=self.webpackChunkluxcars||[]).push([[681],{3681:function(u,n,r){r.r(n),r.d(n,{default:function(){return c}});r(2791);var e=r(3329),c=function(){return(0,e.jsx)("div",{children:"Favorite"})}}}]);
+//# sourceMappingURL=681.da7523d7.chunk.js.map
